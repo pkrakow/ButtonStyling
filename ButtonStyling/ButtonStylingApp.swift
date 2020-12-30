95eb2574117a5dd327b17699038642930401db5b
pkrakow <pkrakow@gmail.com>
2020-12-30 15:57:58 -0800

    Initial Commit

---
+++ ButtonStyling/ButtonStylingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStylingApp.swift
+//  ButtonStyling
+//
+//  Created by Paul Krakow on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStylingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
