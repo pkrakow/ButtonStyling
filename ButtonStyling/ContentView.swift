10822a4ac126501a361043c4aa0f7ef9deba8efd
pkrakow <pkrakow@gmail.com>
2020-12-30 17:08:05 -0800

    Working ButtonStyle tutorial project from https://www.youtube.com/watch?v=vOyx4whZXcU

--- ButtonStyling/ContentView.swift
+++ ButtonStyling/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button("Sample 1") {
+                print("Tapped")
+            }.buttonStyle(FilledRoundedCornerButtonStyle())
+            Button("Sample 2") {
+                print("Tapped")
+            }.buttonStyle(FilledRoundedCornerButtonStyle(font: .body, bgColor: .red, fgColor: .yellow))
+            HStack {
+                Button("Delete") {
+                    
+                }.buttonStyle(SpecialButtonStyle(actionType: .delete))
+                Spacer()
+                Button("Cancel") {
+                    
+                }.buttonStyle(SpecialButtonStyle(actionType: .cancel, withImage: false))
+                Button("OK") {
+                    
+                }.buttonStyle(SpecialButtonStyle(actionType: .confirm))
+            }.padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
